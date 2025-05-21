73923908a6323c4302fb456986f54b8e3329234e
IamCooller <cooller2000@inbox.ru>
2025-05-20 22:17:46 -0600

    Add new section in index.php promoting course benefits with testimonials, reasons for joining, and prerequisites. Enhance layout and styling for improved user engagement.

--- index.php
+++ index.php
@@ -592,6 +592,136 @@
 		</div>
 		<?php get_template_part('template-parts/ui/section-divider'); ?>
 	</section>
+	<section class="py-[57px]  bg-[#20201D] relative overflow-hidden">
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/img/doctor-patient.png" alt="Doctor and patient"
+			class="absolute inset-0 w-full h-full object-cover opacity-10" />
+		<div class="container relative z-[1] ">
+			<h2 class="heading-primary mb-[36px] text-center text-white ">
+				Can't write & publish the book in 4 weeks?</h2>
+			<p class="font-bold font-repo text-lg py-[22px] text-center text-white">That's totally fine!</p>
+			<div class="max-w-[1000px] mx-auto text-white text-center text-sm lg:text-lg font-medium">
+				<p class=" mb-[32px]">In fact, many people don't publish their book in 4 weeks because they have
+					pressing family & work commitments. You can learn the entire system and publish your book at your
+					own pace. It's better to take a bit more time to create a great book than publish something just for
+					the sake of publishing it.</p>
+				<p class="mb-[18px]">The good news is that you'll get:</p>
+
+			</div>
+
+			<div class="grid grid-cols-2 gap-[30px]">
+				<div
+					class="reason-card-bg backdrop-blur-sm rounded-[8px] border border-white/10 py-[23px] text-white px-[37px]">
+					<div class="flex items-center gap-4 mb-[23px]">
+						<div
+							class="reason-icon-bg w-[79px] aspect-square mx-auto drop-shadow-lg flex items-center justify-center rounded-[8px] shrink-0 p-4 ">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/img/infinity-icon.svg"
+								alt="infinity" />
+						</div>
+						<p class="text-[30px] font-bold flex-1">Lifetime Access</p>
+					</div>
+					<p class="text-lg font-medium">You will have lifetime access to all the recordings and bonuses, so
+						you can reference them at a time that's most convenient for you.</p>
+				</div>
+				<div
+					class="reason-card-bg backdrop-blur-sm rounded-[8px] border border-white/10 py-[23px]  text-white px-[37px]">
+					<div class="flex items-center gap-4 mb-[23px]">
+						<div
+							class="reason-icon-bg w-[79px] aspect-square mx-auto drop-shadow-lg flex items-center justify-center rounded-[8px] shrink-0 p-4 ">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/img/repear-icon.svg"
+								alt="repear icon" />
+						</div>
+						<p class="text-[30px] font-bold flex-1">A Repeatable System</p>
+					</div>
+					<p class="text-lg font-medium">You will learn a system that you can repeat later. So even if your
+						first book takes longer than 4 weeks to write and publish, your second one will take you a lot
+						less time (and your third even less).</p>
+				</div>
+			</div>
+
+			<div class="testimonial-card mt-[68px] items-center">
+				<div class="testimonial-avatar">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/people-faces-5.png"
+						alt="People faces #5" />
+				</div>
+
+				<div class="testimonial-content">
+					<p>“Hassan shares his knowledge so <span>you don't have to figure this stuff out by yourself.</span>
+						He is busy like me and has 20 books under his belt, so learning how I can do this "on the side"
+						was important to me. And I will be able to take this information and use it for other books.
+						<span>If you are on the fence, get off and join the course</span> and get your book out there.”
+					</p>
+					<p class="testimonial-author">Kim D. Snyder, Founder of MORE Business in a Box
+					</p>
+				</div>
+			</div>
+			<h2 class="heading-primary mb-[36px] mt-[69px] text-center text-white">
+				What are the prerequisites to join?</h2>
+			<p class="text-center text-white text-lg font-medium">Here are the two mandatory & optional prerequisites to
+				join the course:</p>
+			<div class="grid grid-cols-2 gap-[30px]">
+				<div
+					class="reason-card-bg backdrop-blur-sm rounded-[8px] border border-white/10 py-[23px] text-white px-[37px]">
+					<div class="flex items-center gap-4 mb-[23px]">
+						<div
+							class="reason-icon-bg w-[79px] aspect-square mx-auto drop-shadow-lg flex items-center justify-center rounded-[8px] shrink-0 p-4 ">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/img/kdp-icon.png" alt="kdp" />
+						</div>
+						<p class="text-[30px] font-bold flex-1">KDP Account [Mandatory]</p>
+					</div>
+					<p class="text-lg font-medium">You'll need to open a free Amazon Kindle Direct Publishing (KDP)
+						account. If you're based out of the US, this shouldn't be a problem because you can use your
+						SSN, EIN or TIN. But if you reside outside the US, please confirm you can create an account on
+						the KDP website here (this is for Amazon tax purposes and to ensure you can get paid for your
+						book sales).</p>
+				</div>
+				<div
+					class="reason-card-bg backdrop-blur-sm rounded-[8px] border border-white/10 py-[23px] text-white px-[37px]">
+					<div class="flex items-center gap-4 mb-[23px]">
+						<div
+							class="reason-icon-bg w-[79px] aspect-square mx-auto drop-shadow-lg flex items-center justify-center rounded-[8px] shrink-0 p-4 ">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/img/gpt-icon.svg"
+								alt="gpt icon" />
+						</div>
+						<p class="text-[30px] font-bold flex-1">ChatGPT Plus [Optional]</p>
+					</div>
+					<p class="text-lg font-medium">You'll need an optional subscription to ChatGPT Plus (currently at
+						$20 per month). You don't need to have this because you can follow the course by using a free
+						ChatGPT account, but the free account is usually slow and has messaging limits. (As a side note,
+						you can pay for only 1 month for ChatGPT Plus and then unsubscribe)</p>
+				</div>
+			</div>
+			<div class="testimonial-card mt-[68px] items-center">
+				<div class="testimonial-avatar">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/people-faces-6.png"
+						alt="People faces #6" />
+				</div>
+
+				<div class="testimonial-content">
+					<p>“Taking Hassan’s course has been a transformative experience in my journey as a writer. This
+						course provided me with the structure, motivation, and practical tools I needed to finally start
+						and finish my first book, something I had been dreaming of for years but never knew how to
+						accomplish. <span>My favorite part of the course were the action items, tools, and bonuses he
+							provided weekly.</span> These kept me on track and provided a sense of accomplishment as I
+						progressed. Hassan's expertise, generosity, and the supportive environment he fosters are
+						invaluable.”</p>
+					<p class="testimonial-author">Cenmar Fuertes, Founder & CEO at Sparkle Restoration Services, Inc.
+					</p>
+				</div>
+			</div>
+			<a href="#packages" class="mt-5  sm:mt-6 btn-red mx-auto">Explore
+				Packages</a>
+		</div>
+	</section>
+	<section>
+
+		<div class="rotate-180">
+			<?php get_template_part('template-parts/ui/section-divider'); ?>
+		</div>
+
+		<div class="container">
+
+		</div>
+	</section>
 </main>
 
 <?php get_footer(); ?>
